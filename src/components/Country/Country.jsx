bb71131310142010b1b6af09340f2bbdb0636298
syma sultana <symasultana2002@gmail.com>
2025-02-18 19:21:00 +0600

    add area nad population

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,15 @@
 import './Country.css'
 
 const Country = ({country}) => {
-    const {name ,flags} = country;
+    const {name ,flags , population, area } = country;
     return (
         <div className='Country'>
+
             <h2>Name: {name?.common}</h2>
             <img src={flags.png} alt="" />
+            <h3>Population: {population}</h3>
+            <h3>Area: {area}</h3>
+           
         </div>
     );
 };
