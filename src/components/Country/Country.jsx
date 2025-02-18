64e500a7df186b24705932ddcab57b3b34ca8421
syma sultana <symasultana2002@gmail.com>
2025-02-18 20:24:43 +0600

    edit the button

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import './Country.css'
 
 const Country = ({country}) => {
-    const {name ,flags , population, area } = country;
+    const {name ,flags , population, area,cca3 } = country;
+    //now we declare a set of visited
+    const [visited, setVisited] = useState(false)
+
+    //now we declare a handler for visited
+
+    const handleVisited =()=>{
+        setVisited(!visited);
+    }
     return (
-        <div className='Country'>
+        <div className={`Country ${visited ? 'visited' : 'non-visited'}`}>
 
             <h2>Name: {name?.common}</h2>
             <img src={flags.png} alt="" />
             <h3>Population: {population}</h3>
             <h3>Area: {area}</h3>
+            <h3>Code: {cca3}</h3>
+           <button onClick= {handleVisited}>{ visited ? 'Visited' : 'Going'}</button>
            
         </div>
     );
