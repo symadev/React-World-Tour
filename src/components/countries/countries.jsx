da5bbc752e68ddf271c083c6def3bff1769e67be
syma sultana <symasultana2002@gmail.com>
2025-02-18 19:33:19 +0600

    sort the layout

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../country/country";
+import './Countries.css'
 
 
 const Countries = () => {
@@ -12,12 +13,16 @@ const Countries = () => {
         .then(data=>setCountries(data))
     } ,[])
     return (
+        //this is the parent of country component
         <div>
-        <h3>Countries:{countries.length}</h3>
+        <h2>Countries: {countries.length}</h2>
+        {/* h3 is also the child of div  and there is actually 2 child/element*/}
+        <div className="country-container">
          {
         countries.map(country =>
      <Country key={country.cca3} country= {country}></Country> ) 
-    }  
+    } 
+    </div>
         </div>
     );
 };
