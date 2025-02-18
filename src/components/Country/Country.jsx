bbab7eabfd17a7e3b423d13ce1b380857a412d41
syma sultana <symasultana2002@gmail.com>
2025-02-18 22:03:53 +0600

    edited the visited countries using button handler

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({country ,handleVisitedCountries}) => {
     const {name ,flags , population, area,cca3 } = country;
     //now we declare a set of visited
     const [visited, setVisited] = useState(false)
@@ -11,6 +11,12 @@ const Country = ({country}) => {
     const handleVisited =()=>{
         setVisited(!visited);
     }
+    const passWithParams =()=>{
+        handleVisitedCountries(country);
+
+        // //if we need to add a perameter on handleVisitedCountries handler the we should rapping handleVisitedCountries
+        // with a arrow function
+    }
     return (
         <div className={`Country ${visited ? 'visited' : 'non-visited'}`}>
 
@@ -19,8 +25,9 @@ const Country = ({country}) => {
             <h3>Population: {population}</h3>
             <h3>Area: {area}</h3>
             <h3>Code: {cca3}</h3>
-           <button onClick= {handleVisited}>{ visited ? 'Visited' : 'Going'}</button>
-           
+           <button  style={{ margin: "5px"}}onClick= {handleVisited}>{ visited ? 'Visited' : 'Going'}</button>
+           <br />
+           <button style={{ margin: "5px"}}  onClick= {passWithParams}>Mark As Visited</button>
         </div>
     );
 };
--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -6,22 +6,45 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const[visitedCountries, setVisitedCountries] = useState([]);
 
-    useEffect(() => {
+    const handleVisitedCountries =(country)=>{
+       console.log('add this to your visited countries');
+    //    console.log(country);
+    // visitedCountries.push(country);
+    const newVisitedCountries = [...visitedCountries, country];
+    //using spread operator ,,adding for visited countries
+    setVisitedCountries(newVisitedCountries);
+}
+    
+    
+    
+        useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
         .then(res=> res.json())
         .then(data=>setCountries(data))
     } ,[])
+
+
+
     return (
         //this is the parent of country component
         <div>
         <h2>Countries: {countries.length}</h2>
+        <div>
+            <h2>Visited Countries :  {visitedCountries.length}</h2>
+            <ul>
+
+
+            </ul>
+        </div>
         {/* h3 is also the child of div  and there is actually 2 child/element*/}
         <div className="country-container">
          {
         countries.map(country =>
-     <Country key={country.cca3} country= {country}></Country> ) 
+     <Country key={country.cca3}  handleVisitedCountries={handleVisitedCountries}  country= {country}></Country> ) 
     } 
+    {/* handleVisitedCountries--> as we can set anything as props */}
     </div>
         </div>
     );
